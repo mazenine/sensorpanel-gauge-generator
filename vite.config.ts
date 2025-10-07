5fdfdb72f9d408bddda440dc37e50ff189e82c87
mazenine <juhasz.andrea@gmail.com>
2025-10-07 10:34:20 +0200

    Add refinement base path for versioned build

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,18 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/sensorpanel-gauge-generator/", 
+  base: "/sensorpanel-gauge-generator/v2/",
   server: {
-    open: true 
+    open: true,
   },
   build: {
     outDir: "dist",
     sourcemap: true,
-    target: "esnext"
+    target: "esnext",
   },
   resolve: {
     alias: {
-      "@": "/src"
-    }
-  }
+      "@": "/src",
+    },
+  },
 });
